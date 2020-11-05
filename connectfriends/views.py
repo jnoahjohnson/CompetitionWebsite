d0e27aaa7717a5e7d391af088d4c70225a4c6824
Zach Daniels <zdaniels.ent@gmail.com>
2020-11-05 12:28:28 -0700

    connect friends views

--- connectfriends/views.py
+++ connectfriends/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
 
 def indexPageView(request) :
     return HttpResponse('Connect Friends!') 
+def editConnectFriendsView(request) :
+    return HttpResponse('Edit Group') 
+def createConnectFriendsView(request) :
+    return HttpResponse('Create Group') 
+def joinConnectFriendsView(request) :
+    return HttpResponse('Join Group') 
