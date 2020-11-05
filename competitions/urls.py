05074d1b7f5a269b578fc608827a6ac7f9cbe00b
Noah Johnson <jnoahjohnson@gmail.com>
2020-11-05 12:28:56 -0700

    Add views to competitions

--- competitions/urls.py
+++ competitions/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import indexPageView
+from .views import indexPageView, createCompetitions, editCompetitions, viewCompetitions
 
 urlpatterns = [
     path("", indexPageView, name="index"),
+    path("create/", createCompetitions, name="create"),
+    path("edit/", editCompetitions, name="edit"),
+    path("view/", viewCompetitions, name="view"),
 ]
--- competitions/views.py
+++ competitions/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
 
-def indexPageView(request) :
-    return HttpResponse('Competitions')     
+
+def indexPageView(request):
+    return HttpResponse('Competition Index')
+
+
+def createCompetitions(request):
+    return HttpResponse('Create Competition')
+
+
+def editCompetitions(request):
+    return HttpResponse('Edit Competitions')
+
+
+def viewCompetitions(request):
+    return HttpResponse('View Competition')
