ab61af8f9fa80ea22f593fd337bf4cfdb9f63e6d
tomfife <71194993+tomfife@users.noreply.github.com>
2020-11-05 11:57:46 -0700

    Connect Amigos

--- connectfriends/views.py
+++ connectfriends/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def indexPageView(request) :
-    return HttpResponse('Hello Universe!') 
+    return HttpResponse('Connect Friends!') 
