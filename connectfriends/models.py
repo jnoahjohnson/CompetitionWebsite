07d04dad33b71b3b5782b8a32f90a637c42113e4
Zach Daniels <zdaniels.ent@gmail.com>
2020-11-20 13:56:04 -0700

    connectgroup model

--- connectfriends/models.py
+++ connectfriends/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    Groupname = models.CharField(max_length=30)
+    Username = models.ManyToManyField('leaderboard.User')
+
+    def __str__(self):
+        return self.GroupName
+
