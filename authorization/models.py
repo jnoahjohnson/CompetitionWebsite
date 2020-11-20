2feb7046766532aea9d5e1edd1f9929c895cf809
Nathan Sonnenberg <65731867+ntsonnenberg@users.noreply.github.com>
2020-11-20 13:55:57 -0700

    models for authorization app

--- authorization/models.py
+++ authorization/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    Username = models.CharField(max_length=15)
+    Password = models.CharField(max_length=30)
+    Avatar = models.ImageField()
+    Date_Joined = models.DateField()
+    Login_Attempts = models.IntegerField()
+
+    def __str__(self):
+        return self.Username
