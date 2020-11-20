0e5423783fdcfedbc447edc88a3cb4868dc517f0
Zach Daniels <zdaniels.ent@gmail.com>
2020-11-20 12:35:15 -0700

    connectfriends templates

--- competitions/urls.py
+++ competitions/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import indexPageView, createCompetitions, editCompetitions, viewCompetitions
 
 urlpatterns = [
-    path("create/", createCompetitions, name="create_competition"),
-    path("edit/", editCompetitions, name="edit"),
-    path("view/<int:competition_id>", viewCompetitions, name="competition_view"),
     path("", indexPageView, name="index"),
+    path("create/", createCompetitions, name="create"),
+    path("edit/", editCompetitions, name="edit"),
+    path("view/", viewCompetitions, name="view"),
 ]
--- competitions/views.py
+++ competitions/views.py
@@ -1,49 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-defaultCompetitions =  [
-    {
-        'id': 1,
-        'name': 'Health',
-        'percentComplete': 9,
-        'icon': 'heart',
-        'description': 'Get healthy!!!'
-    },
-    {
-        'id': 2,
-        'name': 'Scripture Study',
-        'percentComplete': 55,
-        'icon': 'book',
-        'description': 'Get spiritual!!!'
-    },
-    {
-         'id': 3,
-        'name': 'Reading',
-        'percentComplete': 21,
-        'icon': 'book',
-        'description': 'Read more!!!'
-    }
-]
 
 
 def indexPageView(request):
-    context = {
-        "competitions": defaultCompetitions
-    }
-
-    return render(request, 'competitions/index.html', context)
+    return render(request, 'competitions/index.html')
 
 
 def createCompetitions(request):
-    return render(request, 'competitions/new_competition.html')
+    return HttpResponse('Create Competition')
 
 
 def editCompetitions(request):
     return HttpResponse('Edit Competitions')
 
 
-def viewCompetitions(request, competition_id):
-    context = {
-        'competitionData': defaultCompetitions[competition_id - 1]
-    }
-    return render(request, 'competitions/view_competition.html', context)
+def viewCompetitions(request):
+    return HttpResponse('View Competition')
