72b6558b64577c36ed2ef1f7ab2911a3345bb035
tomfife <71194993+tomfife@users.noreply.github.com>
2020-11-20 17:41:59 -0700

    Leaderboard

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -4,5 +4,5 @@ from .views import indexPageView, editLeaderboardView, viewLeaderboardView
 urlpatterns = [
     path("", indexPageView, name="index"),
     path("edit/", editLeaderboardView, name="edit"),
-    path("view/", viewLeaderboardView, name="view") 
+    path("view/", viewLeaderboardView, name="leaderboard_view") 
 ]     
