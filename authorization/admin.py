dcae1348c0b28a824d58f0269d16f101444ac6e0
Noah Johnson <jnoahjohnson@gmail.com>
2020-11-20 17:36:06 -0700

    Add templates

--- authorization/admin.py
+++ authorization/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Competitor
 
 # Register your models here.
+admin.site.register(Competitor)
--- connectfriends/urls.py
+++ connectfriends/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import indexPageView, editConnectFriendsView, createConnectFriendsView, joinConnectFriendsView
 
 urlpatterns = [
-    path("", indexPageView, name="index"),
     path("edit/", editConnectFriendsView, name="edit"),
     path("create/", createConnectFriendsView, name="create"),
-    path("join/", joinConnectFriendsView, name="join")
+    path("join/", joinConnectFriendsView, name="join"),
+    path("", indexPageView, name="connectfriends_home")
 ] 
