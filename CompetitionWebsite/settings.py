0398e74c75b618f26f89105b0ac0f283c1642adc
Nathan Sonnenberg <65731867+ntsonnenberg@users.noreply.github.com>
2020-12-01 10:29:01 -0700

    Authorization url mapping update

--- CompetitionWebsite/settings.py
+++ CompetitionWebsite/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'competitionwebsite',
         'USER': 'postgres',
-        'PASSWORD': 'zdanie9',
+        'PASSWORD': 'Tucker01',
         'HOST': 'localhost',
     }
 }
